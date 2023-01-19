52adcc0f62d1b9c6d91192de8a3e432e51072b31
superfree <superfreeeee@gmail.com>
2023-01-19 21:28:22 +0800

    数据结构设计

--- todo-list-app/src/App.jsx
+++ todo-list-app/src/App.jsx
@@ -2,14 +2,50 @@ import React, { FC, useEffect, useState } from 'react';
 import TodoList from './components/TodoList';
 import styles from './App.module.scss';
 
-// interface TodoItem {
-//   count: int;
-//   title: string;
-//   tags: TodoTag[];
-// }
+/**
+ * interface TodoItem {
+ *   completed: boolean;
+ *   title: string;
+ *   tags: TodoTag[];
+ * }
+ */
+
+/**
+ * 1.
+ * interface TodoGroup {
+ *   todoList: TodoItem[]
+ * }
+ */
+
+/**
+ * 2.
+ * interface TodoItem {
+ *   stages
+ *   actions: TodoAction[]
+ *   actions: Set<TodoAction>
+ * }
+ *
+ * interface TodoAction {
+ *   seq: number;
+ *   completed: boolean;
+ *   target: string;
+ *   tags: TodoTag[];
+ * }
+ */
+
+/**
+ * 3.
+ * interface TodoItem {
+ *   completed: boolean;
+ *   target: string;
+ *   tags: TodoTag[];
+ *   stages: TodoItem[];
+ * }
+ */
 
 const App = () => {
   const [todoItems, setTodoItems] = useState([]);
+  // const groups: TodoGroup[];
 
   useEffect(() => {
     fetch('http://localhost:3000/data/todoItems.json')
